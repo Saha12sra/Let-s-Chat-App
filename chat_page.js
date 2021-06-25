a1fa80e4fe409a12cc025ba31453ac55385f11c6
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2021-06-25 15:25:10 +0530

    Add files via upload

--- chat_page.js
+++ chat_page.js
@@ -20,7 +20,19 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
+console.log(firebase_message_id);
+console.log(message_data);
+usersname=message_data['name'];
+likes=message_data['like'];
+message=message_data['message'];
 
+name_with_tag="<h4>"+usersname+"<img src='tick.png' class='user_tick'></h4>";
+message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
+like_button="<button class='btn btn-warning' onclick='update_likes(this.id)' value="+likes+ " id="+firebase_message_id+">";
+span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>Like: "+likes+"</span></button>";
+
+row=name_with_tag+message_with_tag+like_button+span_with_tag;
+document.getElementById("output").innerHTML+=row;
 //End code
       } });  }); }
 getData();
@@ -39,3 +51,24 @@ function send(){
 
       document.getElementById("input_msg").value="";
 }
+
+function update_likes(message_id){
+
+      likes=document.getElementById(message_id).value;
+      updated_likes=Number(likes)+1;
+      firebase.database().ref(room_name).child(message_id).update({
+      
+      like: updated_likes
+      });
+      
+      }
+      
+      function logout(){
+      
+            localStorage.removeItem("username");
+            localStorage.removeItem("roomname");
+            window.location="index.html";
+            
+      }
+      
+      
